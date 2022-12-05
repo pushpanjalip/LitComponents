af55de44a21a1cd78db9d585c821e29c25315039
pooja <pooja.pawar@klm.com>
2022-12-05 14:27:34 +0100

    removed unnecessary code

--- types/Button/custom-button.d.ts
+++ types/Button/custom-button.d.ts
@@ -1,21 +1,10 @@
 import { LitElement } from "lit";
-/**
- * An example element.
- *
- * @slot - This element has a slot
- * @csspart button - The button
- */
+export interface CustomButtonProps {
+    type: string;
+}
 export declare class CustomButton extends LitElement {
-    /**
-     * The number of times the button has been clicked.
-     */
-    text: string;
-    /**
-     * The number of times the button has been clicked.
-     */
     type: string;
     render(): import("lit-html").TemplateResult<1>;
-    private _onClick;
     static styles: import("lit").CSSResult;
 }
 declare global {
